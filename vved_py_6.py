991f583fe00fa547f36f967b0399097be28f2f6a
Kerigan17 <104769289+Kerigan17@users.noreply.github.com>
2022-06-13 14:37:04 +0300

    Update vved_py_6.py

--- vved_py_6.py
+++ vved_py_6.py
@@ -9,16 +9,14 @@ player_two = input("Второй игрок, введи свое имя: ")
 player_three = input("Третий игрок, введи свое имя: ")
 players = [player_one, player_two, player_three]
 
-count_one = 0
-count_two = 0
-count_three = 0
-counts = [count_one, count_two, count_three]
+counts = [0, 0, 0]
 
 player_moves = 0
 
 while win == False:
     word = ""
-    answer = input(f"\n{players[player_moves]}, введите букву: ")
+    print(players[player_moves], "ваш ход! Крутите барабан и называйте букву!")
+    answer = input("введите букву: ")
 
     while len(answer) > 1:
         print("нужно ввести одну букву")
@@ -27,7 +25,7 @@ while win == False:
     if answer in secret_word and answer not in letters:
         print("Вы угадали букву:", answer)
         counts[player_moves] += 1
-        print(f"{players[player_moves]}, вас счет: {counts[player_moves]}")
+        print({players[player_moves]}, "вас счет:", {counts[player_moves]})
     elif answer in letters:
         print("Вы уже использовали эту букву")
     else:
@@ -49,9 +47,5 @@ while win == False:
     if word == secret_word:
         win = True
         print("Игра окончена!")
-        print(f"""\
----{player_one}, ваш счет: {counts[0]}
----{player_two}, ваш счет: {counts[1]}
----{player_three}, ваш счет: {counts[2]}
-    """)
+        print("---", player_one, "ваш счет:", counts[0], "\n---", player_two, "ваш счет:", counts[1], "\n---", player_three, "ваш счет:", counts[2])
         
