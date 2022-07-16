3eb5ca0331d22994af7832ed7691c3cdd5110781
Kerigan17 <104769289+Kerigan17@users.noreply.github.com>
2022-07-16 19:23:10 +0300

    Update 8 lesson.py

--- основы питона/8 lesson.py
+++ основы питона/8 lesson.py
@@ -5,26 +5,26 @@ status = False
 symbols = string.printable.replace(string.whitespace, "")
 
 long_password = int(input("Какой длинны вы хотите пароль? "))
+while long_password < 7 or long_password > 20:
+    print("Пароль должен быть больше 7 и меньше 20 символов.")
+    long_password = int(input("Какой длинны вы хотите пароль? "))
+
 while status == False:
-    if long_password >= 7 and long_password <= 20:
-        password = ""
-        for symbol in range(7):
-            one_symbol = str(random.choice(symbols))
-            password += one_symbol
+    password = ""
+    for symbol in range(7):
+        one_symbol = str(random.choice(symbols))
+        password += one_symbol
 
-        print("Пароль:", password)
-        answer = input("Понравился полученный пароль? ")
+    print("Пароль:", password)
+    answer = input("Понравился полученный пароль? ")
 
-        if answer == "да":
-            status = True
-            print("\nОтлично! Ваш новый пароль:", password)
-        elif answer == "нет":
-            print("\nА если такой? ")
-        else:
-            break
+    if answer == "да":
+        status = True
+        print("\nОтлично! Ваш новый пароль:", password)
+    elif answer == "нет":
+        print("\nА если такой? ")
     else:
-        while long_password < 7 or long_password > 20:
-            print("Пароль должен быть больше 7 и меньше 20 символов.")
-            long_password = int(input("Какой длинны вы хотите пароль? "))
+        break
+        
         
 
